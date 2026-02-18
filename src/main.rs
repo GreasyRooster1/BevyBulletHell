60fc45389dcfef2695399cb033e09dcc8c1f6afc
Dillon Wilson <greasyRooster1@gmail.com>
2026-02-17 19:26:23 -0800

    gen placeholder

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ mod player;
 
 use crate::player::*;
 use bevy::app::App;
+use bevy::asset::RenderAssetUsages;
+use bevy::color::palettes::css;
 use bevy::prelude::*;
+use bevy::render::render_resource::Extent3d;
+use bevy::render::render_resource::{Extent3d, TextureDimension, TextureFormat};
 
 #[derive(Component)]
 pub struct Player;
@@ -17,5 +21,23 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(PlayerPlugin)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+}
+
+fn generate_placeholder_tex(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
+    let mut image = Image::new_fill(
+        Extent3d {
+            width: 32,
+            height: 32,
+        },
+        TextureDimension::D2,
+        &(css::BEIGE.to_u8_array()),
+        TextureFormat::Rgba8UnormSrgb,
+        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+    );
 }
